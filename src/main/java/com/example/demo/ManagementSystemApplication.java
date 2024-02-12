cc02944a79e6bb0fa0a62d49c75df0241e636763
BiancaDavey <biancamdavey@icloud.com>
2024-02-12 19:20:07 +0100

    Add tests for site manager alternate.

--- src/main/java/com/example/demo/ManagementSystemApplication.java
+++ src/main/java/com/example/demo/ManagementSystemApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.enumeration.SiteStatus;
 import com.example.demo.model.Server;
 import com.example.demo.enumeration.Status;
+import com.example.demo.model.Site;
 import com.example.demo.repo.ServerRepo;
+import com.example.demo.repo.SiteRepo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +13,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -20,8 +22,9 @@ public class ManagementSystemApplication {
 		SpringApplication.run(ManagementSystemApplication.class, args);
 	}
 
-	//  Run code after application is initialised.
-	//  Note: servers are example servers, for testing. TODO: look up. Ping external.
+	//  Run code for servers after application is initialised.
+	//  Note: servers are example servers, for testing.
+	///*
 	@Bean
 	CommandLineRunner run(ServerRepo serverRepo){
 		return args -> {
@@ -35,6 +38,24 @@ public class ManagementSystemApplication {
 					"Mail Server", "http://localhost:8080/server/image/server4.png", Status.SERVER_DOWN));
 		};
 	}
+	//*/
+
+	//  TODO: Site manager if using.
+	/*
+	@Bean
+	CommandLineRunner run(SiteRepo siteRepo){
+		return args -> {
+			siteRepo.save(new Site(null, "https://google.com", "ExampleSite1Google",
+					"Main", "http://localhost:8080/site/image/site1.png", SiteStatus.SITE_UP));
+			siteRepo.save(new Site(null, "https://yahoo.com", "ExampleSite2Yahoo",
+					"Main", "http://localhost:8080/site/image/site2.png", SiteStatus.SITE_DOWN));
+			siteRepo.save(new Site(null, "https://www.nationalgeographic.com/", "ExampleSite3NationalGeographic",
+					"Interest", "http://localhost:8080/site/image/site3.png", SiteStatus.SITE_UP));
+			siteRepo.save(new Site(null, "https://biancamdaveyportfolio.wordpress.com/", "ExampleSite4Wordpress",
+					"Portfolio", "http://localhost:8080/site/image/site4.png", SiteStatus.SITE_DOWN));
+		};
+	}
+	*/
 
 	//  CorsError: backend must specify frontend can access it, else error as the two applications are on different domains.
 	@Bean
